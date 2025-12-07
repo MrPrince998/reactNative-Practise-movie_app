5e2deb2e4699cab73d56f76e62d950e85471d1a8
Prince rana <peincerana234@gmail.com>
2025-12-07 22:14:26 +0545

    feat: Integrate NativeWind and Tailwind CSS for styling and initial app structure.

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+
+  return (
+    <View className="flex-1 items-center justify-center">
+      <Text>Hello</Text>
+      <Text className="text-2xl text-accent">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
